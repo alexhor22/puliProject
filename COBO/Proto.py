2fcba575fd9dcf3d3258c055971aff92fcdd3fae
Alejandro Garza <alexhor22@gmail.com>
2019-04-08 19:52:20 -0500

    NO MORE FIREBASE

--- COBO/Proto.py
+++ COBO/Proto.py
@@ -1,8 +1,9 @@
 import pygame
-from firebase import firebase
+import requests
+# from firebase import firebase
 import os
 
-images_path = '/home/pi/Documents/puliProject/COBO/images/'#'C:/Users/aleja/Documents/puliProject/COBO/images/'
+images_path = 'C:/Users/aleja/Documents/puliProject/COBO/images/'#'/home/pi/Documents/puliProject/COBO/images/'#'C:/Users/aleja/Documents/puliProject/COBO/images/'
 
 _image_library = {}
 
@@ -11,7 +12,7 @@ pygame.init()
 screen = pygame.display.set_mode((0, 0),pygame.FULLSCREEN)
 done = False
 clock = pygame.time.Clock()
-pygame.mouse.set_cursor((8,8),(0,0),(0,0,0,0,0,0,0,0),(0,0,0,0,0,0,0,0)) #HIDE CURSOR
+# pygame.mouse.set_cursor((8,8),(0,0),(0,0,0,0,0,0,0,0),(0,0,0,0,0,0,0,0)) #HIDE CURSOR
 screen_width = 0.0
 screen_heigth = 0.0
 screen_width, screen_heigth = pygame.display.get_surface().get_size()
@@ -22,7 +23,7 @@ image_heigth = screen_heigth/2
 distace_between_images = int(screen_width/16)
 distance_from_top = screen_heigth/4
 
-firebase = firebase.FirebaseApplication('https://community-fbbae.firebaseio.com', None)
+# firebase = firebase.FirebaseApplication('https://community-fbbae.firebaseio.com', None)
 
 last_status = 0
 
@@ -71,8 +72,11 @@ if __name__ == '__main__':
         show_main_screen()
         while not done:
                 try:
-                        alert_status = firebase.get('Board/Status',None)
-                        
+                        payload = {'colonia': 'Primavera'} # 'Primavera' is fixed, needs to be changed 
+                        r = requests.get("http://puliapi.herokuapp.com/getStatus", params=payload)
+                        # alert_status = firebase.get('Board/Status',None)
+                        alert_status = r.json()['status']
+
                         if alert_status != last_status: #Cheks if the status changed and if an alert is received it is displayed
                                 main_screen_is_showing = False
                                 if alert_status == 3:                  
@@ -98,21 +102,27 @@ if __name__ == '__main__':
                 		        x,y = pygame.mouse.get_pos()
                                         # print x,y
                                         print x/float(screen_width) , y/float(screen_heigth)
-                                        x = screen_width - x
+                                        # x = screen_width - x
                                         
                                         if main_screen_is_showing:
                                                 if   0.359375 <= y/float(screen_heigth)  and  y/float(screen_heigth) <= 0.75625: #IS IN Y RANGE
                                                         if 0.0479166666667 <= x/float(screen_width) and x/float(screen_width) <= 0.304166666667:
                                                                 screen.blit(screen_resize(get_image(images_path + 'pe_selected.png')),(0,0))
-                                                                firebase.put('Board','Status',2)
+                                                                # firebase.put('Board','Status',2)
+                                                                payload = {'colonia': 'Primavera','status':2}
+                                                                requests.get("http://puliapi.herokuapp.com/changeStatus", params=payload)
                                                                 i_sended_the_alert = True
                                                         elif 0.377083333333 <= x/float(screen_width) and x/float(screen_width) <= 0.635416666667:
                                                                 screen.blit(screen_resize(get_image(images_path + 'suspicious_selected.png')),(0,0))
-                                                                firebase.put('Board','Status',1)
+                                                                # firebase.put('Board','Status',1)
+                                                                payload = {'colonia': 'Primavera','status':1}
+                                                                requests.get("http://puliapi.herokuapp.com/changeStatus", params=payload)
                                                                 i_sended_the_alert = True
                                                         elif 0.691666666667 <= x/float(screen_width) and x/float(screen_width) <= 0.947916666667:
                                                                 screen.blit(screen_resize(get_image(images_path + 'crime_selected.png')),(0,0))
-                                                                firebase.put('Board','Status',3)
+                                                                # firebase.put('Board','Status',3)
+                                                                payload = {'colonia': 'Primavera','status':3}
+                                                                requests.get("http://puliapi.herokuapp.com/changeStatus", params=payload)
                                                                 i_sended_the_alert = True
                                         else:
                                                 if ( 0.816666666667 <= x/float(screen_width) and x/float(screen_width) <= 0.966666666667) and (0.728125<= y/float(screen_heigth) and y/float(screen_heigth) <= 0.953125) and i_sended_the_alert:
@@ -122,7 +132,9 @@ if __name__ == '__main__':
                                                                 screen.blit(screen_resize(get_image(images_path + 'pe_cancelClick.png')),(0,0))
                                                         elif alert_status == 1:
                                                                 screen.blit(screen_resize(get_image(images_path + 's_cancelClick.png')),(0,0))   
-                                                        firebase.put('Board','Status',0)
+                                                        # firebase.put('Board','Status',0)
+                                                        payload = {'colonia': 'Primavera','status':0}
+                                                        requests.get("http://puliapi.herokuapp.com/changeStatus", params=payload)
                                                  
                         last_status = alert_status
 
